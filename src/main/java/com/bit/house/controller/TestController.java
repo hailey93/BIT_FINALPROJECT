5f414f4f381cb7694e757ea0f1f11c8924bdc8c9
MJK0211 <kimminjong0211@gmail.com>
2020-05-29 15:45:33 +0900

    first

---
+++ src/main/java/com/bit/house/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.bit.house.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TestController {
+
+    @GetMapping("/jsp")
+    public String jsp(){
+        return "/jspTest";
+    }
+
+    @GetMapping("/th")
+    public String th(){
+        return "th/thTest";
+    }
+}
