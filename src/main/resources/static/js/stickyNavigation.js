824f1af6332d78697355412748cb80b1b48d4ef2
hailey93 <gkgus1112@gmail.com>
2020-06-03 15:46:04 +0900

    header 만드는중

---
+++ src/main/resources/static/js/stickyNavigation.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {myFunction()};
+
+var sticky = $('.sticky-container').offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        $('.sticky-container').classList.add("fixed-header");
+    } else {
+        $('.sticky-container').classList.remove("fixed-header");
+    }
+}
