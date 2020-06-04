24f7f6eaa3af89d5cf5d5138657dc6a6440c4c2a
hailey93 <gkgus1112@gmail.com>
2020-06-04 15:21:13 +0900

    webconfig img, static 매핑 완료

---
+++ src/main/java/com/bit/house/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.bit.house.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploadImg/**")
+                //.addResourceLocations("file:///D:/img/");
+                .addResourceLocations("classpath:/img/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/");
+
+    }
+
+
+}
--- src/main/java/com/bit/house/mapper/MainMapper.java
+++ src/main/java/com/bit/house/mapper/MainMapper.java
@@ -1,4 +1,11 @@
 package com.bit.house.mapper;
 
+import com.bit.house.domain.ProductVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Mapper
 public interface MainMapper {
+    List<ProductVO> selectMainList();
 }
