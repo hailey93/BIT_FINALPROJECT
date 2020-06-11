a577d229efbc1113d622bba2c8abf145544380f7
MJK0211 <kimminjong0211@gmail.com>
2020-06-11 10:24:35 +0900

    06/10 minjong 최종병합

--- src/main/java/com/bit/house/config/WebConfig.java
+++ src/main/java/com/bit/house/config/WebConfig.java
@@ -14,23 +14,24 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/editor/**")
-                .addResourceLocations("classpath:/editor/");
-        registry.addResourceHandler("/uploadImg/**")
-                //.addResourceLocations("file:///D:/img/");
-                .addResourceLocations("classpath:/img/");
+
         registry.addResourceHandler("/css/**")
                 .addResourceLocations("classpath:/static/css/");
+
         registry.addResourceHandler("/js/**")
                 .addResourceLocations("classpath:/static/js/");
-        /*registry.addResourceHandler("/img/**")
-                .addResourceLocations("classpath:/static/img/");*/
-        registry.addResourceHandler("/img/**")
-                .addResourceLocations("classpath:/img/");
 
-        registry.addResourceHandler("/image/**")
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/");
+
+        registry.addResourceHandler("/icon/**")
+                .addResourceLocations("classpath:/static/icon/");
+
+        registry.addResourceHandler("/uploadImg/**")
                 .addResourceLocations("/image/");
 
+        registry.addResourceHandler("/editor/**")
+                .addResourceLocations("classpath:/editor/");
 
 
     }
