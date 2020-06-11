6873d0d8a95719bdecfa43c796ae4cdab007fd05
hailey93 <gkgus1112@gmail.com>
2020-06-11 10:40:57 +0900

    webconfig img, static 매핑 완료

--- src/main/java/com/bit/house/config/WebConfig.java
+++ src/main/java/com/bit/house/config/WebConfig.java
@@ -1,14 +1,10 @@
 package com.bit.house.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebMvc
--- src/main/java/com/bit/house/mapper/MainMapper.java
+++ src/main/java/com/bit/house/mapper/MainMapper.java
@@ -2,7 +2,6 @@ package com.bit.house.mapper;
 
 import com.bit.house.domain.ProductVO;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Mapper
