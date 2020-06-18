e14a183f19256deabaffcb15cd558d2aa3b7b42e
oleg <olega9320@gmail.com>
2020-06-18 18:17:48 +0900

    20200618

--- src/main/java/com/bit/house/mapper/ProductMapper.java
+++ src/main/java/com/bit/house/mapper/ProductMapper.java
@@ -1,6 +1,9 @@
 package com.bit.house.mapper;
 
+import com.bit.house.domain.ColorVO;
+import com.bit.house.domain.ProductOptionVO;
 import com.bit.house.domain.ProductVO;
+import com.bit.house.domain.SellerVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -19,8 +22,13 @@ public interface ProductMapper {
     @Select("select * from product where categoryCode LIKE CONCAT('%',#{categoryCode},'%')")
     List<ProductVO> selectProductByCategory(String categoryCode);
 
-    @Select("Select * from product where productNo = #{productNo}")
+    @Select("SELECT c.colorType FROM productoption po join color c on po.colorCode=c.colorCode where productno=#{productNo}")
+    List<String> getProductVOByProductColorCode(String productNo);
+
+    @Select("select * from product where productno=#{productNo}")
     ProductVO getProductVOByProductNo(String productNo);
 
+//    @Select("select s.sellerName, s. from product p join seller s on p.sellerName=s.sellerName where productno =#{productNo}")
+//    SellerVO getProductVOBySellerName(String productNo);
 
 }
