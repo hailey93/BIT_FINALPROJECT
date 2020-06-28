23c9705bc319c61bc59cc1af9c95539484ac04a5
MJK0211 <kimminjong0211@gmail.com>
2020-06-29 01:25:59 +0900

    06/29 minjong push

--- src/main/java/com/bit/house/controller/SellerController.java
+++ src/main/java/com/bit/house/controller/SellerController.java
@@ -2,6 +2,7 @@ package com.bit.house.controller;
 
 import com.bit.house.domain.SellerVO;
 import com.bit.house.service.SellerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -9,9 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.security.Principal;
 
+@Slf4j
 @Controller
 public class SellerController {
 
@@ -55,6 +60,27 @@ public class SellerController {
         model.addAttribute("sellerInfo", sellerInfo);
 
         return "th/seller/sellerInfo";
+    }
+
+    @PostMapping("/seller/uploadImg")
+    public String testCategoryCode(MultipartFile[] file, HttpServletRequest request) {
+
+        String uploadFolder = request.getSession().getServletContext().getRealPath("/image/seller");
+
+        for(MultipartFile multipartFile : file) {
+
+            File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+
+            log.info(multipartFile.getOriginalFilename());
+            try {
+                multipartFile.transferTo((saveFile));
+
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+
+        return "redirect:/seller/sellerInfo";
     }
 
 
