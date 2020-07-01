ba7aad1c38b82778b76c45772d68e764f6a68921
Youndae <namyoundae@gmail.com>
2020-07-02 06:42:28 +0900

    0702

---
+++ src/main/resources/static/js/photoList.js
@@ -0,0 +1,45 @@
+
+$(window).scroll(function () {
+    if ($(window).scrollTop()+$(window).height()+30> $(document).height()) {
+
+        $.ajax({
+            url: '/infiniteScrollDown',
+            type: 'GET',
+            dataType: 'JSON',
+
+            success: function (data) {
+
+                var photoList = data.photoList;
+                var infiniteList = '\n';
+
+                $.each(photoList, function (index, value) {
+                    var price=value.customerPrice;
+
+                    infiniteList = infiniteList
+                        + '<div class="col-6 col-md-3 product-item-wrap"><article class="production-item"><a class="production-item__overlay" href="/photodetail/photoBoardNo=' + value.photoBoardNo
+                        + '"></a><div class="production-item-image production-item__image">'
+                        + '<img class="image" alt="" src="/uploadImg/' + value.photoImg1 + '"></div>'
+                        + '<div class="production-item__content"><h1 class="production-item__header">'
+                        + '<span class="production-item__header__name">' + value.photoTitle + '</span></h1>'
+                        + '</div></article></div>'
+
+                });
+
+                $('#autoScroll').append(infiniteList);
+            },
+
+            error: function () {
+                alert('더이상의 사진이 존재하지 않습니다.');
+            }
+        })
+    }
+});
+
+/*
+function enterChat(formName) {
+    window.open("", "new_popup", "scrollbars=no, width=400, height=450, menubar=false, location=false");
+    formName.target = "new_popup";
+    formName.action = "/chat";
+    formName.method = "post";
+    formName.submit();
+}*/
