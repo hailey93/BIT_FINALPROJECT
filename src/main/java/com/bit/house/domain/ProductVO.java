84bbeea5322e7380516119a2cd84b9057293a5ac
hailey93 <gkgus1112@gmail.com>
2020-07-01 09:25:28 +0900

    log 삭제

--- src/main/java/com/bit/house/domain/ProductVO.java
+++ src/main/java/com/bit/house/domain/ProductVO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.Date;
 import java.util.List;
